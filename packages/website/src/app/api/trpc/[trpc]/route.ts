8e570c7edcd18bbc53ec2dce606f5552ca8e6553
Daniil <daniil.o@radency.com>
2023-11-16 00:46:13 +0200

    fix: type

--- packages/website/src/app/api/trpc/[trpc]/route.ts
+++ packages/website/src/app/api/trpc/[trpc]/route.ts
@@ -6,7 +6,7 @@ const handler = (req: Request) =>
     endpoint: "/api/trpc",
     req,
     router: appRouter,
-    createContext: () => ({}),
+    createContext: () => ({ session: null }),
   });
 
 export { handler as GET, handler as POST };
